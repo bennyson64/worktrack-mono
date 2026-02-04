9d4aedefee9fa7cf492fbc6a3d1def5fcb66c35f
BennysonSamuel <bennyson64@gmail.com>
2026-02-04 06:00:59 +0530

    import change

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import * as schema from "./schema/index.ts";
+import * as schema from "./schema.js";
 import "dotenv/config";
 
 const pool = new Pool({
